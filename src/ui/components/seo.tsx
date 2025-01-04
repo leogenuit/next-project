46c08807e77b90ea615300f94cb14fe6a7308fa6
leogenuit <genuitleo@gmail.com>
2025-01-04 15:53:17 +0100

    first commit

---
+++ src/ui/components/seo.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const Seo = ({ title, description }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+    </>
+  );
+};
