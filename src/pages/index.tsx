bc6468d61a1a4e54a26a89e39b31f9870d5a06f6
leogenuit <genuitleo@gmail.com>
2025-01-06 14:42:46 +0100

    design-system, tailwind.config, typography component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,28 @@ export default function Home() {
   return (
     <>
       <Seo title="Aberaise coder" description="Description ..." />
+
       <div>
-        <Typography variant="h1" component="h1" weight="regular">
+        <Typography
+          theme="primary"
+          variant="body-sm"
+          component="p"
+          weight="regular"
+        >
+          Aberaise Coder
+        </Typography>
+        <Typography
+          variant="h1"
+          component="h1"
+          weight="regular"
+          theme="secondary"
+        >
+          Aberaise Coder
+        </Typography>
+        <Typography variant="lead" component="h1" weight="regular">
+          Aberaise Coder
+        </Typography>
+        <Typography variant="caption4" weight="medium" component="h1">
           Aberaise Coder
         </Typography>
       </div>
--- src/ui/design/typography/typography.tsx
+++ src/ui/design/typography/typography.tsx
@@ -32,22 +32,80 @@ export const Typography = ({
   children,
 }: Props) => {
   let variantStyle: string = "";
+  let colorStyle: string = "";
 
   switch (variant) {
     case "display":
-      variantStyle = "text-red-500";
+      variantStyle = "text-8xl";
       break;
     case "h1":
-      variantStyle = "text-green-500";
+      variantStyle = "text-7xl";
+      break;
+    case "h2":
+      variantStyle = "text-6xl";
+      break;
+    case "h3": // Default
+      variantStyle = "text-5xl";
+      break;
+    case "h4":
+      variantStyle = "text-4xl";
+      break;
+    case "h5":
+      variantStyle = "text-3xl";
+      break;
+    case "lead":
+      variantStyle = "text-2xl";
+      break;
+    case "body-lg":
+      variantStyle = "text-lg";
+      break;
+    case "body-base":
+      variantStyle = "text-base";
+      break;
+    case "body-sm":
+      variantStyle = "text-sm";
+      break;
+    case "caption1":
+      variantStyle = "text-caption1";
+      break;
+    case "caption2":
+      variantStyle = "text-caption2";
+      break;
+    case "caption3":
+      variantStyle = "text-caption3";
+      break;
+    case "caption4":
+      variantStyle = "text-caption4";
       break;
   }
+  switch (theme) {
+    case "black": // Default
+      colorStyle = "text-gray";
+      break;
+    case "gray":
+      colorStyle = "text-gray-700";
+      break;
+    case "white":
+      colorStyle = "text-white";
+      break;
+    case "primary":
+      colorStyle = "text-primary";
+      break;
+    case "secondary":
+      colorStyle = "text-secondary";
+      break;
+
+    default:
+      break;
+  }
+
   return (
     <Component
       className={clsx(
         variantStyle,
         weight === "medium" && "font-medium",
         className,
-        theme
+        colorStyle
       )}
     >
       {children}
